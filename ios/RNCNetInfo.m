71c8c910b52815dc32142ddd915984d5235b77d3
garyma <gma@cvent.com>
2021-05-17 11:02:22 -0700

    Fix compile error

--- ios/RNCNetInfo.m
+++ ios/RNCNetInfo.m
@@ -1,4 +1,4 @@
-```/**
+/**
  * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * This source code is licensed under the MIT license found in the
@@ -271,4 +271,3 @@ RCT_EXPORT_METHOD(getCurrentState:(nullable NSString *)requestedInterface resolv
 #endif
 
 @end
-```
